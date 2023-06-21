be01b8cd1090208a0f2ef7be41dd65e9210f4d69
Henry Velasquez <69172276+henryvelas@users.noreply.github.com>
2023-06-20 22:14:34 -0500

    mi primer commit

---
+++ UnitConverter/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace UnitConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbtype = new System.Windows.Forms.ComboBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ltbfrom = new System.Windows.Forms.ListBox();
+            this.ltbto = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtconverted = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(149, 21);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtAmount.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Amount";
+            // 
+            // cbtype
+            // 
+            this.cbtype.FormattingEnabled = true;
+            this.cbtype.Location = new System.Drawing.Point(57, 73);
+            this.cbtype.Name = "cbtype";
+            this.cbtype.Size = new System.Drawing.Size(279, 21);
+            this.cbtype.TabIndex = 2;
+            this.cbtype.SelectedIndexChanged += new System.EventHandler(this.cbtype_SelectedIndexChanged);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(149, 276);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Type";
+            // 
+            // ltbfrom
+            // 
+            this.ltbfrom.FormattingEnabled = true;
+            this.ltbfrom.Location = new System.Drawing.Point(23, 130);
+            this.ltbfrom.Name = "ltbfrom";
+            this.ltbfrom.Size = new System.Drawing.Size(120, 134);
+            this.ltbfrom.TabIndex = 5;
+            // 
+            // ltbto
+            // 
+            this.ltbto.FormattingEnabled = true;
+            this.ltbto.Location = new System.Drawing.Point(228, 130);
+            this.ltbto.Name = "ltbto";
+            this.ltbto.Size = new System.Drawing.Size(120, 134);
+            this.ltbto.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Converted\r\nAmount";
+            // 
+            // txtconverted
+            // 
+            this.txtconverted.Location = new System.Drawing.Point(149, 320);
+            this.txtconverted.Name = "txtconverted";
+            this.txtconverted.Size = new System.Drawing.Size(100, 20);
+            this.txtconverted.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "From";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(225, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "To";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 358);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtconverted);
+            this.Controls.Add(this.ltbto);
+            this.Controls.Add(this.ltbfrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.cbtype);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAmount);
+            this.Name = "Form1";
+            this.Text = "Unit Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbtype;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox ltbfrom;
+        private System.Windows.Forms.ListBox ltbto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtconverted;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ UnitConverter/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnitConverter
+{
+    public partial class Form1 : Form
+    {
+
+        private ConverterService _converterService;
+        public Form1()
+        {
+            InitializeComponent();
+
+            _converterService = new ConverterService();
+
+            cbtype.DataSource =Enum.GetValues(typeof(TypeEnum));
+            cbtype.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            ltbfrom.DataSource = Enum.GetValues(typeof(MassEnum));
+            ltbto.DataSource = Enum.GetValues(typeof(MassEnum));
+        }
+
+        private void cbtype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtAmount.Clear();
+            txtconverted.Clear();
+
+            var type = (TypeEnum)cbtype.SelectedItem;
+            switch (type)
+            {
+                case TypeEnum.Mass:
+                    ltbfrom.DataSource = Enum.GetValues(typeof(MassEnum));
+                    ltbto.DataSource = Enum.GetValues(typeof(MassEnum));
+                    break;
+                case TypeEnum.Temperature:
+                    ltbfrom.DataSource = Enum.GetValues(typeof(TemperatureEnum));
+                    ltbto.DataSource = Enum.GetValues(typeof(TemperatureEnum));
+                    break;
+                case TypeEnum.Time:
+                    ltbfrom.DataSource = Enum.GetValues(typeof(TimeEnum));
+                    ltbto.DataSource = Enum.GetValues(typeof(TimeEnum));
+                    break;
+                default: break;
+                 
+            }
+
+        }
+
+       
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (txtAmount.Text.Length <= 0)
+                return;
+
+            if (ltbfrom.SelectedItems == ltbto.SelectedItems)
+            {
+                txtconverted.Text = txtAmount.Text;
+            }
+
+            var type = (TypeEnum)cbtype.SelectedItem;
+            double amount = double.Parse(txtAmount.Text);
+            double convertedAmout = 0;
+
+            switch (type)
+            {
+                case TypeEnum.Mass:
+                    var massFrom = (MassEnum)ltbfrom.SelectedItems[0];
+                    var massTo = (MassEnum)ltbto.SelectedItems[0];
+                    convertedAmout= _converterService.ConvertMassUnit(massFrom, massTo,amount);
+                    break; 
+                case TypeEnum.Temperature:
+                    var temperatureFrom = (TemperatureEnum)ltbfrom.SelectedItems[0];
+                    var temperatureTo = (TemperatureEnum)ltbto.SelectedItems[0];
+                    convertedAmout =  _converterService.ConvertTemperatureUnit(temperatureFrom, temperatureTo, amount);
+                    break; 
+                case TypeEnum.Time:
+                    var TimeFrom = (TimeEnum)ltbfrom.SelectedItems[0];
+                    var TimeTo = (TimeEnum)ltbto.SelectedItems[0];
+                    convertedAmout = _converterService.ConvertTimeUnit(TimeFrom, TimeTo, amount);
+                    break;
+                default: break;
+            }
+
+            txtconverted.Text = convertedAmout.ToString();
+
+
+
+        }
+    }
+}
